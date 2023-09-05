fe66463aff9178ad5590712249311e4b88d6aa7b
HANEUL ROH <haneulroh@HANEULs-MacBook-Air.local>
2023-09-05 18:53:26 -0400

    added

--- sequelize/config/config.js
+++ sequelize/config/config.js
@@ -8,6 +8,12 @@ module.exports ={
     database: DB_NAME,
     host: DB_HOST,
     dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   },
   test: {
     username: TEST_DB_USERNAME,
@@ -15,6 +21,12 @@ module.exports ={
     database: TEST_DB_NAME,
     host: TEST_DB_HOST,
     dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   },
   production: {
     username: DB_USERNAME,
