8293f43ef9a68a1ccde2b887210ae42bd0686e0a
SKY ROH <skyroh@SKYs-Laptop.local>
2024-06-10 10:50:58 -0400

    back to normal

--- models/index.js
+++ models/index.js
@@ -1,32 +1,24 @@
-import fs from 'fs';
-import path from 'path';
-import { Sequelize } from 'sequelize';
-import { fileURLToPath } from 'url';
-import config from '../config/config.js';
-import pg from 'pg';
+'use strict';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+const process = require('process');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const dbConfig = config[env];
+const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 
 let sequelize;
-if (dbConfig.use_env_variable) {
-  sequelize = new Sequelize(process.env[dbConfig.use_env_variable], {
-    ...dbConfig,
-    dialectModule: pg,
-  });
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, {
-    ...dbConfig,
-    dialectModule: pg,
-  });
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
-fs.readdirSync(__dirname)
-  .filter((file) => {
+fs
+  .readdirSync(__dirname)
+  .filter(file => {
     return (
       file.indexOf('.') !== 0 &&
       file !== basename &&
@@ -34,12 +26,12 @@ fs.readdirSync(__dirname)
       file.indexOf('.test.js') === -1
     );
   })
-  .forEach((file) => {
+  .forEach(file => {
     const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
     db[model.name] = model;
   });
 
-Object.keys(db).forEach((modelName) => {
+Object.keys(db).forEach(modelName => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
@@ -48,4 +40,4 @@ Object.keys(db).forEach((modelName) => {
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
-export default db;
+module.exports = db;
