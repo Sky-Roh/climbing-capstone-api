2d3fd95552fba86a45d6a21b40058cefd2e11768
SKY ROH <skyroh@SKYs-Laptop.local>
2024-06-10 10:18:19 -0400

    changed config.js to make it work on vercel

--- config/config.js
+++ config/config.js
@@ -1,4 +1,46 @@
-import "dotenv/config"
+// require("dotenv").config();
+// const {
+//   DB_HOST,
+//   DB_USERNAME,
+//   DB_PASSWORD,
+//   DB_NAME,
+//   TEST_DB_HOST,
+//   TEST_DB_PASSWORD,
+//   TEST_DB_NAME,
+//   TEST_DB_USERNAME,
+// } = process.env;
+
+// module.exports = {
+//   development: {
+//     username: DB_USERNAME,
+//     password: DB_PASSWORD,
+//     database: DB_NAME,
+//     host: DB_HOST,
+//     dialect: "postgres",
+//     dialectOptions: {
+//       ssl: {
+//         require: true,
+//         rejectUnauthorized: false,
+//       },
+//     },
+//   },
+//   test: {
+//     username: TEST_DB_USERNAME,
+//     password: TEST_DB_PASSWORD,
+//     database: TEST_DB_NAME,
+//     host: TEST_DB_HOST,
+//     dialect: "postgres",
+//   },
+//   production: {
+//     username: DB_USERNAME,
+//     password: DB_PASSWORD,
+//     database: "database_production",
+//     host: DB_HOST,
+//     dialect: "postgres",
+//   },
+// };
+
+import "dotenv/config";
 
 const {
   DB_HOST,
@@ -11,7 +53,7 @@ const {
   TEST_DB_USERNAME,
 } = process.env;
 
-module.exports = {
+export default {
   development: {
     username: DB_USERNAME,
     password: DB_PASSWORD,
@@ -35,7 +77,7 @@ module.exports = {
   production: {
     username: DB_USERNAME,
     password: DB_PASSWORD,
-    database: "database_production",
+    database: DB_NAME,
     host: DB_HOST,
     dialect: "postgres",
   },
